ce710f807513bc28f56eedb012d7b21f6d896d4e
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2021-09-24 20:14:33 -0300

    Adicionei os dots e inclui o vídeo

--- src/Caroussel.js
+++ src/Caroussel.js
@@ -2,11 +2,27 @@ import './Caroussel.css';
 import {useState} from 'react';
 import {FcNext, FcPrevious} from 'react-icons/fc';
 import {VscCircleFilled, VscCircleOutline} from 'react-icons/vsc'
+import arcdetriomphe from './assets/videos/napoleon00.mp4'
 
 const importAll = (r) => r.keys().map(r); //Função para importar todos as imagens
 
-const images = importAll(require.context('./assets/images', false, /\.(jpg)$/)); //Importo todas as imagens jpg
-const num_imgs = images.length-1
+const importImages = importAll(require.context('./assets/images', false, /\.(jpg)$/)); //Importo todas as imagens jpg
+
+const images = importImages.reduce((lista,image)=>{
+  lista.push(
+    {
+      src:image.default,
+      type:"image/jpg"
+    }
+  );
+  return lista
+},[]);
+
+const video = {src:arcdetriomphe,type:"video/mp4"};
+
+const medias = [...images,video];
+
+const num_medias = medias.length-1;
 
 export const Caroussel = () => {
 
@@ -14,38 +30,56 @@ export const Caroussel = () => {
 
   const nextPosition = () => {
     //Função para andar para frente no Caroussel
-    setPosition(position===num_imgs?0:position+1);
+    setPosition(position===num_medias?0:position+1);
   }
 
   const previousPosition = () => {
     //Função para andar para trás no Caroussel
-    setPosition(position===0?num_imgs:position-1);
+    setPosition(position===0?num_medias:position-1);
   }
 
-  const PlotImages = () =>
-    //Elemento para imprimir as imagens na tela
-    images.map((image,index) =>{
+  const DotIndicator = ({num, setPosition}) => {
+    //Elemento que imprime os dots e adiciona a funcionalidade setSlide
+    const setSlide = () => {
+      //Função para selecionar o slide pelos dots
+      setPosition(num);
+    }
+    if (num===position){
+      return <VscCircleFilled className="dots"/>
+    }else {
+      return <VscCircleOutline className="dots" onClick={setSlide}/>
+    }
+  }
+
+  const PlotSlides = () =>
+    //Elemento para imprimir os slides na tela
+    medias.map(({src,type},index) =>{
       if (index===position){
-        return <img src={image.default} key = {index} alt={`napoleon${index+1} fate`}/>
+        if (type==="image/jpg"){
+          return <img src={src} className="slide" key={index} alt={`napoleon${index+1} fate`}/>
+        } else {
+          return (
+          <video controls>
+            <source src={src} className="slide" type={type} key={index}/>
+            Your browser does not support the video tag.
+          </video>
+          )
+        }
       } else {
         return null
       }})
 
   const PlotDotIndicator = () =>
     //Elemento para imprimir os pontos na tela
-    images.map((_,index) => {
-      if (index===position){
-        return <VscCircleFilled/>
-      }else {
-        return <VscCircleOutline/>
-      }
-      })
+    <div className="indicator">
+      {medias.map((_,index) => <DotIndicator num={index} setPosition={setPosition} />)}
+    </div>
 
   return (
     <div className='slider'>
       <FcPrevious className="arrow left" onClick={previousPosition}/>
       <FcNext className="arrow right" onClick={nextPosition}/>
-      <PlotImages/>
+      <PlotSlides/>
       <PlotDotIndicator/>
     </div>
   )
