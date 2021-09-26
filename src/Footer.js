b2d2184bcb9a53ba22015330395c6b89f3f005a4
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2021-09-26 12:24:00 -0300

    Adicionei um footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <h1>Créditos</h1>
+            <a href="https://br.freepik.com/vetores/quadro">moldura criada por Creative_hat - br.freepik.com</a>
+        </div>
+    ) 
+}
+
+export default Footer
+
