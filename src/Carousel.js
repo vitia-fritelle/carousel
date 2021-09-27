68d4a11a7c695ed6a819bb94faeb77297cc3a19f
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2021-09-27 10:03:31 -0300

    Organização de código
    Organizei a parte de percorrer os slides e ainda adicionei uma
    props ao Carousel para receber os slides, o que deixa o componente
    mais flexível.

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,48 +2,15 @@ import './Carousel.css';
 import {useEffect, useState} from 'react';
 import {FcNext, FcPrevious} from 'react-icons/fc';
 import {VscCircleFilled, VscCircleOutline} from 'react-icons/vsc'
-import arcdetriomphe from './assets/videos/napoleon00.mp4'
 
-const importAll = (r) => r.keys().map(r); //Função para importar todos as imagens
 
-const importImages = importAll(require.context('./assets/images/napoleao', false, /\.(jpg)$/)); //Importo todas as imagens jpg
-
-const images = importImages.reduce((lista,image)=>{
-  lista.push(
-    {
-      src:image.default,
-      type:"image/jpg"
-    }
-  );
-  return lista
-},[]);
-
-const video = {src:arcdetriomphe,type:"video/mp4"};
-
-const medias = [...images,video];
-
-const num_medias = medias.length-1;
-
-const listaSlides = medias.map(({src,type},index) =>{
-  //Crio os slides que serão apresentados e guardo num array
-  if (type==="image/jpg"){
-    return <img src={src} className="slide" key={index} alt={`napoleon${index+1} fate`}/>
-  } else if (type==="video/mp4"){
-    return (
-    <video autoPlay>
-      <source src={src} className="slide" type={type} key={index}/>
-      Your browser does not support the video tag.
-    </video>
-    )
-  } else {
-    return null
-  }
-});
-
-const Carousel = ({interval, direction}) => {
+const Carousel = ({interval, direction='up', slides}) => {
   //Elemento de slider Carousel
 
+  const num_medias = slides.length-1;
+
   const [position,setPosition] = useState(0);
+  const [flag,setFlag] = useState(0);
 
   const previousPosition = () => {
     //Função para andar para trás no Carousel
@@ -55,103 +22,78 @@ const Carousel = ({interval, direction}) => {
     setPosition(position===num_medias?0:position+1);
   }
 
-  return (
-    <div className='slider'>
-      {interval && 
-      <HandlingSetInterval 
-      interval={interval} 
-      next={nextPosition} 
-      prev={previousPosition} 
-      position={position} 
-      setPosition={setPosition}
-      direction={direction}/>
-      }
-      <ArrowLeft previous={previousPosition}/>
-      <ArrowRight next={nextPosition}/>
-      <PlotSlides position={position} slides={listaSlides}/>
-      <PlotDotIndicator 
-      position={position} 
-      setPosition={setPosition} 
-      slides={medias}/>
-    </div>
-  )
-}
-
-const HandlingSetInterval = ({next,prev,position,setPosition,interval,direction='up'}) => {
-  //Elemento utilizado para manipular a aplicação do Hook ou não
-  //Também é utilizado para adicionar algumas funcionalidades a mais 
-  //de como percorrer o Carousel
-  const [flag,setFlag] = useState(0);
-  
-  const flagBearer = () =>{
-    //Função para levantar ou abaixar a flag
-    if(position===0) setFlag(0);
-    else if (position===num_medias) setFlag(1);
-  }
-
   const upDownPosition = () => {
     //Função para andar para frente e para trás no Carousel
     if (flag===0) {
-      next();
+      nextPosition();
     } else if (flag===1) {
-      prev();
+      previousPosition();
     }
-  }
-
-  const getRandomIntInclusive = () => {
-    //Função que escolhe um número aleatório entre min e max inclusive
-    const min = Math.ceil(0);
-    const max = Math.floor(num_medias);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   const randomPosition = () => {
     //Função para andar aleatoriamento no Carousel
+    const getRandomIntInclusive = () => {
+      //Função que escolhe um número aleatório entre min e max inclusive
+      const min = Math.ceil(0);
+      const max = Math.floor(num_medias);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
     setPosition(getRandomIntInclusive);
   }
 
-  const directions = {
-    //Usando factory aqui
-    'up':next,
-  'down':prev,
-  'updown':upDownPosition,
-  'random':randomPosition
-  };
+  const action = {
+    'up':nextPosition,
+    'down':previousPosition,
+    'updown':upDownPosition,
+    'random':randomPosition
+  }
 
   useEffect(() => {
+    const flagBearer = () =>{
+      //Função para levantar ou abaixar a flag
+      if(position===0) setFlag(0);
+      else if (position===num_medias) setFlag(1);
+    }
     (direction==='updown') && flagBearer();
-    const timer = setInterval(directions[direction], interval);
+  })
+
+  return (
+    <div className='slider'>
+      {interval && <HandlingSetInterval interval={interval} action={action[direction]}/>}
+      <ArrowLeft previous={previousPosition}/>
+      <ArrowRight next={nextPosition}/>
+      <PlotSlides position={position} slides={slides}/>
+      <PlotDotIndicator 
+      position={position} 
+      setPosition={setPosition} 
+      slides={slides}/>
+    </div>
+  )
+}
+
+const HandlingSetInterval = ({interval,action}) => {
+  //Elemento utilizado para manipular a aplicação do Hook useEffect
+  useEffect(() => {
+    const timer = setInterval(action, interval);
     return () => {clearInterval(timer);}
   })
   return null
 }
 
-const ArrowLeft = ({previous}) => {
+const ArrowLeft = ({previous}) => <FcPrevious className="arrow left" onClick={previous}/>
   //Elemento de flecha para esquerda
-  return (
-    <FcPrevious className="arrow left" onClick={previous}/>
-  )
-}
 
-const ArrowRight = ({next}) => {
+const ArrowRight = ({next}) => <FcNext className="arrow right" onClick={next}/>
   //Elemento de flecha para direita
-  return (
-    <FcNext className="arrow right" onClick={next}/>
-  )
-}
-
+    
 const PlotSlides = ({position,slides}) => slides[position]
   //Elemento para imprimir os slides na tela
-  //Caso queira trocar o interior dos slides, 
-  //devo mexer aqui e fazer provavelmente um novo .map
   
-
 const DotIndicator = ({num,position,setPosition}) => {
   //Elemento que imprime os dots e adiciona a funcionalidade setSlide
-  const setSlide = () => {
+  const setSlide = () => {setPosition(num);}
     //Função para selecionar o slide pelos dots
-    setPosition(num);
-  }
   if (num===position){
     return <VscCircleFilled className="dots"/>
   }else {
--- src/index.js
+++ src/index.js
@@ -4,9 +4,46 @@ import Carousel from './Carousel';
 import Footer from './Footer';
 import './index.css'
 
+import arcdetriomphe from './assets/videos/napoleon00.mp4'
+
+const importAll = (r) => r.keys().map(r); //Função para importar todos as imagens
+
+const importImages = importAll(require.context('./assets/images/napoleao', false, /\.(jpg)$/)); //Importo todas as imagens jpg
+
+const images = importImages.reduce((lista,image)=>{
+  lista.push(
+    {
+      src:image.default,
+      type:"image/jpg"
+    }
+  );
+  return lista
+},[]);
+
+const video = {src:arcdetriomphe,type:"video/mp4"};
+
+const medias = [...images,video];
+
+const listaSlides = medias.map(({src,type},index) =>{
+  //Crio os slides que serão apresentados e guardo num array
+  if (type==="image/jpg"){
+    return <img src={src} className="slide" key={index} alt={`napoleon${index+1} fate`}/>
+  } else if (type==="video/mp4"){
+    return (
+    <video autoPlay>
+      <source src={src} className="slide" type={type} key={index}/>
+      Your browser does not support the video tag.
+    </video>
+    )
+  } else {
+    return null
+  }
+});
+
+
 ReactDOM.render(
     <>
-      <Carousel interval={1000} direction='random' />
+      <Carousel interval={1000} direction='random' slides={listaSlides} />
       <Footer/>
     </>,
   document.getElementById('root')
