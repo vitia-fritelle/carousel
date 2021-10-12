ea584f71df0649cb07f3f56c9f4d32f13e2596aa
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2021-10-12 13:00:09 -0300

    Refatoração do código e organização
    Tirei a criação dos slides dessa parte e os coloquei
    num outro arquivo.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,16 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './Carousel';
 import Footer from './Footer';
-import './index.css'
-
-import arcdetriomphe from './assets/videos/napoleon00.mp4'
-
-const importAll = (r) => r.keys().map(r); //Função para importar todos as imagens
-
-const importImages = importAll(require.context('./assets/images/napoleao', false, /\.(jpg)$/)); //Importo todas as imagens jpg
-
-const images = importImages.reduce((lista,image)=>{
-  lista.push(
-    {
-      src:image.default,
-      type:"image/jpg"
-    }
-  );
-  return lista
-},[]);
-
-const video = {src:arcdetriomphe,type:"video/mp4"};
-
-const medias = [...images,video];
-
-const listaSlides = medias.map(({src,type},index) =>{
-  //Crio os slides que serão apresentados e guardo num array
-  if (type==="image/jpg"){
-    return <img src={src} className="slide" key={index} alt={`napoleon${index+1} fate`}/>
-  } else if (type==="video/mp4"){
-    return (
-    <video autoPlay>
-      <source src={src} className="slide" type={type} key={index}/>
-      Your browser does not support the video tag.
-    </video>
-    )
-  } else {
-    return null
-  }
-});
-
+import listaSlides from './Slides';
 
 ReactDOM.render(
     <>
-      <Carousel interval={1000} direction='random' slides={listaSlides} />
+      <Carousel 
+       interval={1000} 
+       direction='updown' 
+       slides={listaSlides}/>
       <Footer/>
     </>,
   document.getElementById('root')
